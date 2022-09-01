6fe9560400925f28b77c41416203052bd38e0361
masonwilson <mew8wjn@virginia.edu>
2022-09-01 15:58:10 -0400

    adding different orbit speeds
    adding 3 new scripts with differing orbit speeds, there's probably a better way to do this but idk what it is :)

--- Assets/Scripts/ObjectRotatorSlowest.cs
+++ Assets/Scripts/ObjectRotatorSlowest.cs
@@ -1,20 +1,20 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectRotator : MonoBehaviour
+public class ObjectRotatorSlowest : MonoBehaviour
 {
-    float orbitSpeed = .2f;
+    float orbitSpeed = .1f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3 (0, orbitSpeed, 0));
+        transform.Rotate(new Vector3(0, orbitSpeed, 0));
     }
 }
